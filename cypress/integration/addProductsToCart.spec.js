79763424be9ba6da27884f4d31e5c17ad4975d73
marcos <marcos.morais@ab-inbev.com>
2020-04-02 11:30:29 -0300

    make add buttons id unique

---
+++ cypress/integration/addProductsToCart.spec.js
@@ -0,0 +1,21 @@
+// const email = 'marcos.morais@ab-inbev.com';
+// const password = 'tanamao';
+// const appHomeUrl = 'http://b2blearningv2.herokuapp.com/';
+// const appLoginUrl = 'http://b2blearningv2.herokuapp.com/login';
+
+// describe('Adding products to cart', function() {
+//   it('Increase qtdy of one product and check if it be on the cart', () => {
+//     cy.visit('http://b2blearningv2.herokuapp.com/');
+//     cy.get()
+//   });
+
+//   it('After make login redirects to / page', () => {
+//     cy.get('#button-entrar-navbar').click();
+//     cy.get('#input-home-01').type(email);
+//     cy.get('#input-home-02').type(password);
+//     cy.get('#button-entrar-drawer').click();
+//     cy.location().should(location => {
+//       expect(location.href).to.eq(appHomeUrl);
+//     });
+//   });
+// });
