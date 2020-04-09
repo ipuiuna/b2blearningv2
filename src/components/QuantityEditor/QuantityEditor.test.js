6e52d36a9d9a9a26643acb36e4a5275204cb6a01
marcos <marcos.morais@ab-inbev.com>
2020-04-09 13:10:41 -0300

    fixing QuantityEditor tests

--- src/components/QuantityEditor/QuantityEditor.test.js
+++ src/components/QuantityEditor/QuantityEditor.test.js
@@ -12,22 +12,23 @@ describe('<QuantityEditor />', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<QuantityEditor />);
+    wrapper = shallow(<QuantityEditor buttonsId={1} />);
   });
 
   it('Should have two buttons', () => {
+    console.log(wrapper.debug());
     expect(wrapper.find(Button)).toHaveLength(2);
   });
 
   it('Should have a button to inc items', () => {
-    expect(wrapper.find({ id: 'button-incItem' })).toHaveLength(1);
+    expect(wrapper.find({ id: 'button-incItem-1' })).toHaveLength(1);
   });
 
   it('Should have a button to dec items', () => {
-    expect(wrapper.find({ id: 'button-decItem' })).toHaveLength(1);
+    expect(wrapper.find({ id: 'button-decItem-1' })).toHaveLength(1);
   });
 
   it('Should have a field to displays quantitys', () => {
-    expect(wrapper.find({ id: 'field-quantity' })).toHaveLength(1);
+    expect(wrapper.find({ id: 'field-quantity-1' })).toHaveLength(1);
   });
 });
